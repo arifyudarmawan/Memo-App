65c67835426bda4d6fd137c714d9b622ce25b997
pradiptaagus <igedeaguspradipta@gmail.com>
2018-11-07 17:42:43 +0800

    add tab list and fragment

--- app/src/main/java/com/example/pradiptaagus/app_project4/Activity/AddMemoActivity.java
+++ app/src/main/java/com/example/pradiptaagus/app_project4/Activity/AddMemoActivity.java
@@ -1,15 +1,27 @@
 package com.example.pradiptaagus.app_project4.Activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.pradiptaagus.app_project4.Api.ApiInterface;
+import com.example.pradiptaagus.app_project4.Fragment.HomeFragment;
 import com.example.pradiptaagus.app_project4.R;
 
-public class AddMemoActivity extends AppCompatActivity {
+public class AddMemoActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText etMemoTitle;
+    private EditText etMemoDetail;
+    private Button btnSave;
+    private Button btnDiscard;
+    private ApiInterface apiInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +36,51 @@ public class AddMemoActivity extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                startActivity(new Intent(getApplicationContext(), HomeFragment.class));
             }
         });
 
+        // declare share preference
+        final SharedPreferences userPreference = this.getSharedPreferences("user", Context.MODE_PRIVATE);
+
+        // get token from shared preference
+        String token = userPreference.getString("token", "missing");
+
+        // get user input
+        etMemoTitle = (EditText) findViewById(R.id.et_memo_title);
+        etMemoDetail = (EditText) findViewById(R.id.et_memo_detail);
+
+        btnSave = (Button) findViewById(R.id.btn_save);
+        btnSave.setOnClickListener(this);
+
+        btnDiscard = (Button) findViewById(R.id.btn_discard);
+        btnDiscard.setOnClickListener(this);
+
+        if (token == "missing") {
+            loginActivity();
+        }
+    }
+
+    private void loginActivity() {
+        startActivity(new Intent(this, LoginActivity.class));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_save:
+                Toast.makeText(this, "save", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_discard:
+                //Toast.makeText(this, "discard", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/pradiptaagus/app_project4/Activity/LoginActivity.java
+++ app/src/main/java/com/example/pradiptaagus/app_project4/Activity/LoginActivity.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +17,7 @@ import android.widget.Toast;
 
 import com.example.pradiptaagus.app_project4.Api.ApiClient;
 import com.example.pradiptaagus.app_project4.Api.ApiInterface;
+import com.example.pradiptaagus.app_project4.Fragment.HomeFragment;
 import com.example.pradiptaagus.app_project4.Model.LoginResponse;
 import com.example.pradiptaagus.app_project4.R;
 
@@ -23,12 +25,14 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btnLogin;
-    private TextView signup;
+    private TextView tvSignup;
     private EditText etEmail;
     private EditText etPassword;
     private ApiInterface apiInterface;
+    private ProgressBar progressBar;
+    private SharedPreferences userPreference;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -36,83 +40,85 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // get EditText element
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(this);
+        tvSignup = findViewById(R.id.tv_go_to_signup_view);
+        tvSignup.setOnClickListener(this);
 
-        etEmail = (EditText) findViewById(R.id.et_email);
-        etPassword = (EditText) findViewById(R.id.et_password);
-        btnLogin = (Button) findViewById(R.id.btn_login);
-        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.pb_login);
+        // get element progressbar
+        progressBar = findViewById(R.id.pb_login);
+        // set progressbar invisible
         progressBar.setVisibility(View.INVISIBLE);
-        final SharedPreferences userPreference = this.getSharedPreferences("user", Context.MODE_PRIVATE);
+
         // get token from shared preference
+        userPreference = this.getSharedPreferences("user", Context.MODE_PRIVATE);
         String token = userPreference.getString("token", "missing");
 
-
         if (token != "missing") {
-            homeActivity();
+            loginActivity();
         }
-
-        // login
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                progressBar.setVisibility(View.VISIBLE);
-                //Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT).show();
-                String email = etEmail.getText().toString();
-                String password = etPassword.getText().toString();
-
-                // call login method
-                login(email, password);
-            }
-
-            private void login(String email, String password) {
-                ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
-                Call<LoginResponse> call = apiInterface.login(email, password);
-
-                call.enqueue(new Callback<LoginResponse>() {
-                    @Override
-                    public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
-                        //Toast.makeText(getApplicationContext(), ""+response.body().isStatus(),Toast.LENGTH_SHORT).show();
-                        if (response.body().isStatus()) {
-                            progressBar.setVisibility(View.INVISIBLE);
-                            Toast.makeText(getApplicationContext(), response.body().getMessage(), Toast.LENGTH_SHORT).show();
-                            Toast.makeText(getApplicationContext(), response.body().getToken(), Toast.LENGTH_SHORT).show();
-                            // save token to shared preference
-                            SharedPreferences.Editor editor = userPreference.edit();
-                            editor.putString("token", response.body().getToken());
-                            editor.apply();
-
-                            // go to home activity
-                            homeActivity();
-                        } else {
-                            progressBar.setVisibility(View.INVISIBLE);
-                            Toast.makeText(getApplicationContext(), response.body().getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<LoginResponse> call, Throwable t) {
-                        progressBar.setVisibility(View.INVISIBLE);
-                        Toast.makeText(getApplicationContext(), "Connection error", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-
-
-
-        // go to sign up view
-        signup = (TextView) findViewById(R.id.tv_go_to_signup_view);
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
-    private void homeActivity() {
-        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+    private void loginActivity() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_login:
+                login();
+                break;
+
+            case R.id.tv_go_to_signup_view:
+                signup();
+                break;
+        }
+    }
+
+    private void signup() {
+        startActivity(new Intent(this, SignUpActivity.class));
+    }
+
+    private void login() {
+        progressBar.setVisibility(View.VISIBLE);
+        //Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT).show();
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+        Call<LoginResponse> call = apiInterface.login(email, password);
+
+        call.enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                //Toast.makeText(getApplicationContext(), ""+response.body().isStatus(),Toast.LENGTH_SHORT).show();
+                if (response.body().isStatus()) {
+                    progressBar.setVisibility(View.INVISIBLE);
+                    Toast.makeText(getApplicationContext(), response.body().getMessage(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), response.body().getToken(), Toast.LENGTH_SHORT).show();
+                    // save token to shared preference
+                    SharedPreferences.Editor editor = userPreference.edit();
+                    editor.putString("token", response.body().getToken());
+                    editor.apply();
+
+                    // go to home activity
+                    loginActivity();
+                } else {
+                    progressBar.setVisibility(View.INVISIBLE);
+                    Toast.makeText(getApplicationContext(), response.body().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+                progressBar.setVisibility(View.INVISIBLE);
+                Toast.makeText(getApplicationContext(), "Connection error", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
